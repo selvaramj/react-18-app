c7e07eb2236384b3e4cd49d6ca720b7415fc0642
selvaramj <jselvamanu123@gmail.com>
2023-03-11 21:48:58 +0530

    Learned few basics and refresh the some react core concepts..

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import ListGroup from './components/ListGroup/ListGroup';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const data = <h1>Hello World</h1>;
+  const cities = [
+    'New York',
+    'Sans fransico',
+    'California',
+    'London',
+    'Tokyo',
+  ].sort();
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ListGroup heading="Cities" items={cities} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ListGroup/ListGroup.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+
+interface Props {
+  heading: string;
+  items: string[];
+}
+const ListGroup = ({ heading, items }: Props) => {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //const array = ['New York', 'Sans fransico', 'California', 'London', 'Tokyo'];
+  return (
+    <>
+      <h2>{heading}</h2>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={`list-group-item ${selectedIndex == index && 'active'}`}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ListGroup;
+// Under the hood - with help of babel - JS compiler converts the JSX to react elements
+// import React from 'react';
+
+// const ListGroup = () => {
+//   return React.createElement(
+//     React.Fragment,
+//     null,
+//     React.createElement('h2', null, 'List Group'),
+//     React.createElement(
+//       'ul',
+//       {
+//         className: 'list-group',
+//       },
+//       React.createElement(
+//         'li',
+//         {
+//           className: 'list-group-item',
+//         },
+//         'An item',
+//       ),
+//       React.createElement(
+//         'li',
+//         {
+//           className: 'list-group-item',
+//         },
+//         'A second item',
+//       ),
+//       React.createElement(
+//         'li',
+//         {
+//           className: 'list-group-item',
+//         },
+//         'A third item',
+//       ),
+//       React.createElement(
+//         'li',
+//         {
+//           className: 'list-group-item',
+//         },
+//         'A fourth item',
+//       ),
+//       React.createElement(
+//         'li',
+//         {
+//           className: 'list-group-item',
+//         },
+//         'And a fifth one',
+//       ),
+//     ),
+//   );
+// };
+// export default ListGroup;
